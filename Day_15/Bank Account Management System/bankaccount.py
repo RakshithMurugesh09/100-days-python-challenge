9a090ce99aac39c587d9ca48d8c4adf6f57853c8
Rakshith Murugesh <rakshith.09m@gmail.com>
2026-07-12 10:00:06 +0530

    Day_15 Banking project inital file

---
+++ Day_15/Bank Account Management System/bankaccount.py
@@ -0,0 +1,48 @@
+class BankAccount:
+    """
+    Represents a bank account.
+    """
+
+    def __init__(self):
+        """
+        TODO:
+        - Ask for account holder name
+        - Ask for account number
+        - Ask for initial balance
+        - Store values using self
+        """
+        pass
+
+    def deposit(self):
+        """
+        TODO:
+        - Ask user for deposit amount
+        - Validate amount
+        - Update balance
+        """
+        pass
+
+    def withdraw(self):
+        """
+        TODO:
+        - Ask user for withdrawal amount
+        - Check available balance
+        - Withdraw if possible
+        """
+        pass
+
+    def check_balance(self):
+        """
+        TODO:
+        - Print current balance
+        """
+        pass
+
+    def display_details(self):
+        """
+        TODO:
+        - Display account holder
+        - Display account number
+        - Display balance
+        """
+        pass
---
+++ Day_15/Bank Account Management System/main.py
@@ -0,0 +1,55 @@
+from art import logo
+from bank_account import BankAccount
+
+
+def display_menu():
+    """
+    TODO:
+    Display:
+
+    1. Deposit
+    2. Withdraw
+    3. Check Balance
+    4. Display Account Details
+    5. Exit
+    """
+    pass
+
+def main():
+
+    # TODO:
+    # Print logo
+
+    # TODO:
+    # Create one BankAccount object
+
+    # TODO:
+    # Run program loop
+
+        # TODO:
+        # Display menu
+
+        # TODO:
+        # Get user choice
+
+        # TODO:
+        # Deposit
+
+        # TODO:
+        # Withdraw
+
+        # TODO:
+        # Check Balance
+
+        # TODO:
+        # Display Details
+
+        # TODO:
+        # Exit Program
+
+        # TODO:
+        # Invalid Choice
+
+
+if __name__ == "__main__":
+    main()
