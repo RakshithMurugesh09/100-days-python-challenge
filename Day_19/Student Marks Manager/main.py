70d2bdc9798f2070d126838070aae4c16d05f06e
Rakshith Murugesh <rakshith.09m@gmail.com>
2026-08-01 07:34:10 +0530

    Day 19: Student Marks Manager intial file

---
+++ Day_19/Student Marks Manager/main.py
@@ -0,0 +1,19 @@
+import csv
+
+def add_student():
+    pass
+
+def view_students():
+    pass
+
+def search_student():
+    pass
+
+def calculate_average():
+    pass
+
+def display_menu():
+    pass
+
+def main():
+    pass
