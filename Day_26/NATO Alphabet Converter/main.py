812884a9e0090ec6ecf7620f3a35d2360a677e6b
Rakshith Murugesh <rakshith.09m@gmail.com>
2026-08-12 18:08:15 +0530

    Initialize Day 26 NATO Alphabet Converter project

---
+++ Day_26/NATO Alphabet Converter/main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+data = pd.read_csv("nato_phonetic_alphabet.csv")
+
+# TODO:
+# 1. Convert dataframe to dictionary
+# 2. Ask user for a word
+# 3. Generate NATO code words using list comprehension
+# 4. Print the result
+
