522e1e572c847d9a20ce297b065ee346bc9f8ef1
Rakshith Murugesh <rakshith.09m@gmail.com>
2026-09-22 17:12:57 +0530

    Add FlightSearch class using SerpAPI

---
+++ Day_39/Part 1 - Cheap Flight Finder/flight_search.py
@@ -0,0 +1,46 @@
+from serpapi import GoogleSearch
+from datetime import datetime, timedelta
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from flight_data import FlightData
+
+
+class FlightSearch:
+
+    def search_flight(self,departure_id,arrival_id):
+
+        tomorrow = datetime.now() + timedelta(days=1)
+        six_months = datetime.now() + timedelta(days=180)
+
+        params = {
+            "engine": "google_flights",
+            "departure_id": departure_id,
+            "arrival_id": arrival_id,
+            "outbound_date": tomorrow.strftime("%Y-%m-%d"),
+            "return_date": (tomorrow + timedelta(days=7)).strftime("%Y-%m-%d"),
+            "currency": "INR",
+            "hl": "en",
+            "api_key": os.getenv("SERPAPI_API_KEY")
+        }
+
+        search = GoogleSearch(params)
+
+        results = search.get_dict()
+
+        flights = results.get("best_flights", [])
+
+        if not flights:
+            return None
+
+        cheapest = flights[0]
+
+        return FlightData(
+            price=cheapest["price"],
+            origin_city=departure_id,
+            destination_city=arrival_id,
+            departure_date=params["outbound_date"],
+            return_date=params["return_date"]
+        )
